54fbe8400cbd06f6c3180b98111ee54fb034a876
uufree <369470777@qq.com>
2018-03-08 17:40:49 +0800

    fix some

---
+++ data/3/list.h
@@ -0,0 +1,84 @@
+/*************************************************************************
+	> File Name: list.h
+	> Author: uuchen
+	> Mail: 1319081676@qq.com
+	> Created Time: 2018年03月08日 星期四 16时53分43秒
+ ************************************************************************/
+
+#ifndef _LIST_H
+#define _LIST_H
+
+#define NULL (void*)0
+
+template <typename T>
+struct ListNode
+{
+    T _data;
+    ListNode<T>* _prev;
+    ListNode<T>* _next;
+    
+    ListNode(){};
+    ListNode(T data_,ListNode<T>* prev_=NULL,ListNode<T>* next_=NULL) :
+        _data(data_),
+        _prev(prev_),
+        _next(next_)
+    {};
+    
+    ListNode<T>* insertAsPrev(T const& data_);
+    ListNode<T>* insertAsNext(T const& data_);
+};
+
+template<typename T>
+class List
+{
+    public:
+        List(){init();};
+        List(List<T> const& list_);
+        List(List<T> const& list_,int rank_,int size_);
+        List(ListNode<T> const& nodes_,int size_);
+        ~List();
+
+    //read interface
+        int size(){return _size;};
+        bool empty(){return _size == 0;};
+        T& operator=(int rank_) const;
+        ListNode<T>* first() const {return _header->_next;};
+        ListNode<T>* last() const {return _tailer->_prev;};
+        bool isSort() const;
+        ListNode<T>* find(T& data_) const{return find(_header,_size,data_);};
+        ListNode<T>* find(ListNode<T>* nodes_,int size_,T& data_) const; 
+        ListNode<T>* search(T& data_) const{return search(_header,_size,data_);};
+        ListNode<T>* search(ListNode<T>* nodes_,int size_,T& data_) const;
+        ListNode<T>* max(ListNode<T>* nodes_,int size_) const;
+        ListNode<T>* max() const{return max(_header,_size);};
+        ListNode<T>* min(ListNode<T>* nodes_,int size_) const;
+        ListNode<T>* min() const{return min(_header,_size);};
+    
+    //write interface
+        ListNode<T>* insertAsFirst(T& data_);
+        ListNode<T>* insertAsLast(T& data_);
+        ListNode<T>* insertAsPrev(ListNode<T>* nodes_,T& data_);
+        ListNode<T>* insertAsNext(ListNode<T>* nodes_,T& data_);
+        T remove(ListNode<T>* nodes_);
+        void sort(ListNode<T>* nodes_,int size_);
+        void sort(){sort(_header,_size);};
+        int deduplicate();
+        int uniquify();
+
+
+    protected:
+        void init();
+        void clear();
+        void copyNotes(ListNode<T>* nodes_,int size_);
+        void merge(ListNode<T>* first_,int size1_,List<T>& list_,ListNode<T>* second_,int size2_);
+        void mergeSort(ListNode<T>* nodes_,int size_);
+        void selectionSort(ListNode<T>* nodes_,int size_);
+        void insertSort(ListNode<T>* nodes_,int size_);
+    private:
+        int _size;
+        ListNode<T>* _header;
+        ListNode<T>* _tailer;
+};
+
+
+#endif
