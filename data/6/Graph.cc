2290dd51cda54de01edc878b609a11607cc0bfae
uufree <369470777@qq.com>
2018-03-23 08:35:42 +0800

    add Graph

--- data/6/Graph.cc
+++ data/6/Graph.cc
@@ -8,19 +8,106 @@
 #include"Graph.h"
 
 template<typename Tv,typename Te>
-class GraphMatrix : public Graph<Tv,Te>
+void Graph<Tv,Te>::bfs(int s)
 {
-    public:
-        GraphMatrix(){Graph<Tv,Te>::_n = Graph<Tv,Te>::_e = 0;}
-        ~GraphMatrix()
-        {
-            for(int i=0;i<Graph<Tv,Te>::_n;i++)
-                for(int j=0;j<Graph<Tv,Te>::_n;j++)
-                    delete E[i][j];
-        }
+    reset();
+    int clock = 0;int v = s;
     
-    private:
-        Vector<Vectex<Tv> > V;
-        Vector<Vector<Edge<Te>*>> E;  
-};
+    do
+        if(status(v) == UNDISCOVERED)
+            BFS(v,clock);
+    while(s != (v = (++v % Graph<Tv,Te>::_n)));
+}
+
+template<typename Tv,typename Te>
+void Graph<Tv,Te>::BFS(int v,int& clock)
+{
+    Deque<int> deque;
+    status(v) = DISCOVERED;
+    deque.push(v);
+    
+    while(!deque.empty())
+    {
+        int v = deque.pop();
+        dTime(v) = ++clock;
+        for(int u = firstNBR(v);u;u = nextNBR(v,u))
+            if(status(u) == UNDISCOVERED)
+            {
+                status(u) = DISCOVERED;
+                deque.push(u);
+                type(v,u) = TREE;
+                parent(u) = v;
+            }
+            else
+                type(v,u) = CROSS;
+        status(v) = VISITED;
+    }
+}
+
+template<typename Tv,typename Te>
+void Graph<Tv,Te>::dfs(int s) 
+{
+    reset();
+    int clock = 0;int v = s;
+
+    do
+        if(status(v) == UNDISCOVERED)
+            DFS(v,clock);
+    while(s != (v = (++v % Graph<Tv,Te>::_n)));
+}
+
+template<typename Tv,typename Te>
+void Graph<Tv,Te>::DFS(int v,int& clock)
+{
+    dTime(v) = ++clock;
+    status(v) = DISCOVERED;
+
+    for(int u=firstNBR(v);u>-1;u=nextNBR(v))
+        switch(status(u))
+        {
+            case UNDISCOVERED:
+                type(v,u) = TREE;
+                parent(u) = v;
+                DFS(u,clock);
+                break;
+            case DISCOVERED:
+                type(v,u) = BACKWORD;
+                break;
+            default:
+                type(v,u) = (dTime(v) < dTime(u)) ? FORWARD : CROSS;
+                break;
+        }
+
+    status(v) = VISITED;
+    fTime(v) = ++clock;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- data/6/Graph.h
+++ data/6/Graph.h
@@ -10,9 +10,10 @@
 
 #include"../2/vector.h"
 #include"../4/stack.h"
+#include"../4/deque.h"
 #define INT_MAX 65536
 
-typedef enum {UNDISCOVERED,DISCOVERED,VISTED} PointStatus;//点状态
+typedef enum {UNDISCOVERED,DISCOVERED,VISITED} PointStatus;//点状态
 typedef enum {UNDETERMINED,TREE,CROSS,FORWARD,BACKWORD} LineType;//边在遍历树中所属的类型
 
 //定点类型,边类型
@@ -46,7 +47,7 @@ class Graph
         virtual int inDegree(int) = 0;//返回顶点V的出度
         virtual int outDegree(int) = 0;//返回顶点V的入度
         virtual int firstNBR(int) = 0;//顶点V的首个临接顶点
-        virtual int nextNBR(int) = 0;//顶点V的下一个临接顶点
+        virtual int nextNBR(int,int) = 0;//顶点V的下一个临接顶点
         virtual PointStatus& status(int) = 0;//返回顶点V的状态
         virtual int& dTime(int) = 0;//顶点V的时间标签dTime
         virtual int& fTime(int) = 0;//顶点V的时间标签fTime
@@ -98,6 +99,97 @@ struct Edge
         _data(data),_weight(weight),_type(UNDETERMINED)
     {};
 };
+
+template<typename Tv,typename Te>
+class GraphMatrix : public Graph<Tv,Te>
+{
+    public:
+        GraphMatrix(){Graph<Tv,Te>::_n = Graph<Tv,Te>::_e = 0;}
+        ~GraphMatrix()
+        {
+            for(int i=0;i<Graph<Tv,Te>::_n;i++)
+                for(int j=0;j<Graph<Tv,Te>::_n;j++)
+                    delete E[i][j];
+        }
+        
+        //点操作    
+        Tv& vertex(int i) override {return V[i];}; 
+        int inDegree(int i) override {return V[i]._inDegree;};
+        int outDegree(int i) override {return V[i]._outDegree;};
+        int firstNBR(int i) override {return nextNBR(i,Graph<Tv,Te>::_n);};
+        int nextNBR(int i,int j) override//在临接表中很好描述
+        {
+            while((j > -1) && (!exists(i,--j)));
+            return j;
+        };
+        PointStatus& status(int i) override {return V[i]._status;}; 
+        int& dTime(int i) override {return V[i]._dTime;}; 
+        int& fTime(int i) override {return V[i]._fTime;};
+        int& parent(int i) override {return V[i]._parent;};
+        int& priority(int i) override {return V[i]._priority;};
+        
+        int insert(Tv const& data) override
+        {
+            for(int i=0;i<Graph<Tv,Te>::_n;i++)
+                E[i].insert(NULL);
+            Graph<Tv,Te>::_n++;
+            E.insert(Vector<Edge<Te>*>(Graph<Tv,Te>::_n,Graph<Tv,Te>::_n,(Edge<Te>*)NULL));
+            return V.insert(Vectex<Tv>(data));
+        }
+        
+        Te remove(int i) override
+        {
+            for(int j=0;j<Graph<Tv,Te>::_n;j++)
+                if(exists(i,j))
+                {
+                    delete E[i][j];//删除所有出边
+                    --V[j]._outDegree;
+                }
+            E.remove(i);
+            Graph<Tv,Te>::_n--;
+            Tv data = V[i]._data;
+            V.remove(i);
+
+            for(int j=0;j<Graph<Tv,Te>::_n;j++)
+                if(Edge<Te>* edge = E[j].remove(i))
+                {
+                    delete edge;
+                    V[j]._inDegree--;
+                }
+            return data;
+        }
+        
+        //边操作     
+        bool exists(int i,int j) override {return i>=0 & i<Graph<Tv,Te>::_n & j>=0 & j<Graph<Tv,Te>::_n & E[i][j]!=NULL;}
+        LineType& tyep(int i,int j) override {return E[i][j]->_type;};
+        Te& edge(int i,int j) override {return E[i][j]->_data;};
+        int& weight(int i,int j) override {return E[i][j]->_weight;};
+        
+        void insert(Te const& elem,int w,int i,int j) override
+        {
+            if(exists(i,j))
+                return;
+            E[i][j] = new Edge<Te>(elem,w);
+            Graph<Tv,Te>::_e++;
+            V[i]._outDegree++;
+            V[j]._inDegree++;
+        }
+        
+        Te remove(int i,int j) override
+        {
+            Te data = E[i][j]->_data;
+            delete E[i][j];
+            E[i][j] = NULL;
+            Graph<Tv,Te>::_e--;
+            V[i]._outDegree--;V[j]._inDegree--;
+            return data;
+        };
+
+    private:
+        Vector<Vectex<Tv> > V;
+        Vector<Vector<Edge<Te>*>> E;  
+};
+
 
 
 #endif
