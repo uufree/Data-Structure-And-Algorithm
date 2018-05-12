2f46814e0299ab1bc8fb79291856f76636700d0d
uufree <369470777@qq.com>
2018-05-13 06:38:26 +0800

    add some

--- offer/16.cc
+++ offer/16.cc
@@ -25,6 +25,24 @@ void prevOrder(struct BinaryTreeNode* node)
     }
 }
 
+void prevOrder_r(struct BinaryTreeNode* node)
+{
+    std::stack<BinaryTreeNode*> stack;
+    stack.push(node);
+
+    while(!stack.empty())
+    {
+        struct BinaryTreeNode* temp = stack.top();
+        stack.pop();
+        std::cout << temp->value << std::endl;
+
+        if(temp->right)
+            stack.push(temp->right);
+        if(temp->left)
+            stack.push(temp->left);
+    }
+}
+
 void midOrder(struct BinaryTreeNode* node)
 {
     if(node)
@@ -35,6 +53,28 @@ void midOrder(struct BinaryTreeNode* node)
     }
 }
 
+void midOrder_r(struct BinaryTreeNode* node)
+{
+    std::stack<BinaryTreeNode*> stack;
+    while(1)
+    {
+        while(node)
+        {
+            stack.push(node);
+            node = node->left;
+        }
+        
+        if(stack.empty())
+            break;
+
+        node = stack.top();
+        stack.pop();
+        std::cout << node->value << std::endl;
+        node = node->right;
+        
+    }
+}
+
 void postOrder(struct BinaryTreeNode* node)
 {
     if(node)
@@ -42,6 +82,109 @@ void postOrder(struct BinaryTreeNode* node)
         postOrder(node->left);
         postOrder(node->right);
         std::cout << node->value << std::endl;
+    }
+}
+
+/////////////////////////////////////////////
+//54
+void midOrder_rs(BinaryTreeNode* node,int index)
+{
+    std::stack<BinaryTreeNode*> stack;
+
+    while(1)
+    {
+        while(node)
+        {
+            stack.push(node);
+            node = node->left;
+        }
+
+        if(stack.empty())
+            break;
+        --index;
+        node = stack.top();
+        stack.pop();
+        if(index == 0)
+        {
+            std::cout << node->value << std::endl;
+            break;
+        }
+        node = node->right;
+    }
+}
+
+///////////////////////////////////////////
+//55-01
+
+int deepth(BinaryTreeNode* node)
+{
+    if(node == NULL)
+        return 0;
+    int left = deepth(node->left);
+    int right = deepth(node->right);
+
+    return left > right ? left+1 : right + 1;
+}
+
+///////////////////////////////////////
+//55-02
+
+bool isBlanced(BinaryTreeNode* node,int* deepth)
+{
+    if(node == NULL)
+    {
+        deepth = 0;
+        return true;
+    }
+    
+    int left;
+    int right;
+    if(isBlanced(node->left,&left) && isBlanced(node->right,&right))
+    {
+        int diff = left - right;
+        if(diff <= 1 && diff >= -1)
+        {
+            *deepth = 1 + (left > right ? left : right);
+            return true;
+        }
+    }
+    return false;
+}
+
+bool isBlanced(BinaryTreeNode* node)
+{
+    if(node == NULL)
+        return false;
+    int deepth = 0;
+    return isBlanced(node,&deepth);
+}
+
+//////////////////////////////////////////
+//56-01
+//利用异或的性质求只出现一次的数字
+
+//////////////////////////////////////////
+//57
+
+void findPair(int* array,int length,int key)
+{
+    if(array == NULL || length <= 0)
+        return;
+
+    int* start = array;
+    int* end = array+length-1;
+
+    while(start < end)
+    {
+        if(*start + *end < key)
+            ++head;
+        else if(*start + *end == key)
+        {
+            std::cout << *start << "    " << *end << std::endl;
+            break;
+        }
+        else
+            --end;
     }
 }
 
@@ -69,7 +212,8 @@ int main(int argc,char** argv)
     array[6].value = 8;
     array[6].left = NULL;
     array[6].right = NULL;
-
-    midOrder(&array[0]);
+    
+    int arrays[8] = {1,2,3,4,5,6,7,8};
+    findPair(arrays,8,7);
     return 0;
 }
